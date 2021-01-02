7b42e55588dd0cfe592f38c76a14d54f9d2b4a68
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-01-02 15:47:55 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -26,29 +26,29 @@ int main(int argc, string argv[]) // argc = number of arguments, argv[] the posi
     }
     key = atoi(argv[1]);
     string plaintext = get_string("Text:\n");
-    string cyphertext = malloc(strlen(plaintext)); //Alocate (strlen(plaintext) memory to the cyphertext string.
+    string ciphertext = malloc(strlen(plaintext)); //Alocate (strlen(plaintext) memory to the ciphertext string.
     string alphaindex = malloc(strlen(plaintext)); //Alocate (strlen(plaintext) memory to the alphaindex string.
     for (int i = 0, n = strlen(plaintext); i < n; i++) // Go through the whole string.
     {
         if (isupper(plaintext[i])) //Check if it's a uppercase letter.
         {
             alphaindex[i] = plaintext[i] - 65; // Convert to alphabetical index in order to aply the formula down below.
-            cyphertext[i] = (alphaindex[i] + key) % 26; // Formula that cycles the alphabet when reaching the end, instead of going further.
-            cyphertext[i] = cyphertext[i] + 65; // Convert from alphabetical index to ASCII in order to represent real letters.
+            ciphertext[i] = (alphaindex[i] + key) % 26; // Formula that cycles the alphabet when reaching the end, instead of going further.
+            ciphertext[i] = ciphertext[i] + 65; // Convert from alphabetical index to ASCII in order to represent real letters.
         }
         else if (islower(plaintext[i]))  //Check if it's a lowercase letter.
         {
             alphaindex[i] = plaintext[i] - 97;
-            cyphertext[i] = (alphaindex[i] + key) % 26;
-            cyphertext[i] = cyphertext[i] + 97;
+            ciphertext[i] = (alphaindex[i] + key) % 26;
+            ciphertext[i] = ciphertext[i] + 97;
 
         }
         else
         {
-            cyphertext[i] = cyphertext[i] + plaintext[i]; //If it's not a letter, then keep it as it is.
+            ciphertext[i] = ciphertext[i] + plaintext[i]; //If it's not a letter, then keep it as it is.
         }
     }
-    printf("%s\n", cyphertext);
-    free(cyphertext); // Release the allocated memory to prevent memory leaks.
+    printf("%s\n", ciphertext);
+    free(ciphertext); // Release the allocated memory to prevent memory leaks.
     free(alphaindex); // Release the allocated memory to prevent memory leaks.
 }
