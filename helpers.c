4a6e126cfb05e473b3b6a844ba4a6e97f7067e77
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-03-13 22:23:37 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
             int k = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
 
@@ -93,9 +93,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 {
                     if ((i + k >= 0 && i + k < height) && (j + l >= 0 && j + l < width))
                     {
-                        sumRed = sumRed + temp[i+k][j+l].rgbtRed;
-                        sumGreen = sumGreen + temp[i+k][j+l].rgbtGreen;
-                        sumBlue = sumBlue + temp[i+k][j+l].rgbtBlue;
+                        sumRed = sumRed + temp[i + k][j + l].rgbtRed;
+                        sumGreen = sumGreen + temp[i + k][j + l].rgbtGreen;
+                        sumBlue = sumBlue + temp[i + k][j + l].rgbtBlue;
                         counter++;
                     }
                 }
