661a55529d91b7b723516bf5c886bdfc62d75c1d
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-03-23 14:56:38 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -12,14 +12,10 @@ int main(int argc, char *argv[])
     BYTE buffer[512];
     char imgnumber[8];
     FILE *img;
-    if (argc != 2)
-    {
-        return 1;
-    }
     FILE *card = fopen(argv[1], "r");
-    while(fread(&buffer, 512, 1, card) == 1)
+    while (fread(&buffer, 512, 1, card) == 1)
     {
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             found = true;
             if (counter == 0)
@@ -39,7 +35,7 @@ int main(int argc, char *argv[])
             }
 
         }
-        else if(found == true)
+        else if (found == true)
         {
             fwrite(&buffer, 512, 1, img);
         }
