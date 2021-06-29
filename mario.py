5ce89374f31b8a69e552e409aa4a7858b9b955fd
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-06-29 08:23:01 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -1,4 +1,10 @@
-size = int(input("Height: "))
+import cs50
+
+while True:
+    size = cs50.get_int("Height: ")
+    if(size > 0 and size < 9):
+        break
+
 for x in range(size + 1):
     print(" " * (size - x), end="")
     print("#" * x, end="")
