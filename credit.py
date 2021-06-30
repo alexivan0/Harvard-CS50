717946db279503daf122ca84792582f778ca20e3
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-06-30 10:23:23 +0000

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -6,7 +6,6 @@ strnumber = str(number)
 checksum = 0
 muliply = 0
 reversenumber = strnumber[::-1]
-print(reversenumber)
 for i in range(1,len(reversenumber),2):
     multiply = int(reversenumber[i]) * 2
     if len(str(multiply)) == 2:
@@ -15,16 +14,14 @@ for i in range(1,len(reversenumber),2):
         checksum = checksum + multiply
 for i in range(0,len(reversenumber),2):
     checksum = checksum + int(reversenumber[i])
-print(len(strnumber))
-print(int((strnumber)[0:2]))
 
 
 
-if len(strnumber) == 15 and ((strnumber)[0:2] == str(34) or (strnumber)[0:2] == str(37)):
+if checksum % 10 = 0 and len(strnumber) == 15 and ((strnumber)[0:2] == str(34) or (strnumber)[0:2] == str(37)):
     print("AMEX")
-elif len(strnumber) == 16 and (50 < int((strnumber)[0:2]) < 56):
+elif checksum % 10 = 0 and len(strnumber) == 16 and (50 < int((strnumber)[0:2]) < 56):
     print("MASTERCARD")
-elif (len(strnumber) == 13 or len(strnumber) == 16) and (strnumber)[0] == str(4):
+elif checksum % 10 = 0 and (len(strnumber) == 13 or len(strnumber) == 16) and (strnumber)[0] == str(4):
     print("VISA")
 else:
     print("INVALID")
