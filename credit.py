0e8248ec2d31ecddec519fcdfa651e95900c5a97
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-06-30 10:13:21 +0000

    automated commit by check50 [check50=True]

--- credit.py
+++ credit.py
@@ -11,20 +11,18 @@ for i in range(1,len(reversenumber),2):
     multiply = int(reversenumber[i]) * 2
     if len(str(multiply)) == 2:
         checksum = checksum + int(str(multiply)[0]) + int(str(multiply)[1])
-        print(checksum)
     else:
         checksum = checksum + multiply
-        print(checksum)
-print(checksum)
 for i in range(0,len(reversenumber),2):
     checksum = checksum + int(reversenumber[i])
-print(checksum)
+print(len(strnumber))
+print(int((strnumber)[0:2]))
 
 
 
 if len(strnumber) == 15 and ((strnumber)[0:2] == str(34) or (strnumber)[0:2] == str(37)):
     print("AMEX")
-elif str(strnumber) == 16 and (str(50) < (strnumber)[0:2] < str(56)):
+elif len(strnumber) == 16 and (50 < int((strnumber)[0:2]) < 56):
     print("MASTERCARD")
 elif (len(strnumber) == 13 or len(strnumber) == 16) and (strnumber)[0] == str(4):
     print("VISA")
