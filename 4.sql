db428b3223773a8dd85fbad4725fc9cbec18f5ef
xtonyxmacaronyx <xtonyxmacaronyx@users.noreply.github.com>
2021-07-12 18:33:11 +0000

    automated commit by check50 [check50=True]

--- 4.sql
+++ 4.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating = 10.0;
+SELECT COUNT(title) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE rating = 10.0;
